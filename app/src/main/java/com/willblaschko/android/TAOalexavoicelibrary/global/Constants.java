0b43a7d27f8dc375ae728ad0e33296006bf1009a
liutao00 <liutao00@outlook.com>
2017-05-27 10:02:01 +0800

    Initial commit

---
+++ app/src/main/java/com/willblaschko/android/TAOalexavoicelibrary/global/Constants.java
@@ -0,0 +1,9 @@
+package com.willblaschko.android.TAOalexavoicelibrary.global;
+
+/**
+ * @author will on 5/30/2016.
+ */
+
+public class Constants {
+    public static final String PRODUCT_ID = "Horn_alexa_android";
+}
